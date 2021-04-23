6f11845dc83f184d19fe4cb876bc5ec98cd78064
xybhym <76042891+xybhym@users.noreply.github.com>
2021-04-23 18:37:19 +0800

    Update jd_fruit.js

--- jd_fruit.js
+++ jd_fruit.js
@@ -1245,29 +1245,29 @@ function timeFormat(time) {
   }
   return date.getFullYear() + '-' + ((date.getMonth() + 1) >= 10 ? (date.getMonth() + 1) : '0' + (date.getMonth() + 1)) + '-' + (date.getDate() >= 10 ? date.getDate() : '0' + date.getDate());
 }
-function readShareCode() {
-  return new Promise(async resolve => {
-    $.get({url: `http://jd.turinglabs.net/api/v2/jd/farm/read/${randomCount}/`, timeout: 10000,}, (err, resp, data) => {
-      try {
-        if (err) {
-          console.log(`${JSON.stringify(err)}`)
-          console.log(`${$.name} API请求失败，请检查网路重试`)
-        } else {
-          if (data) {
-            console.log(`随机取个${randomCount}码放到您固定的互助码后面(不影响已有固定互助)`)
-            data = JSON.parse(data);
-          }
-        }
-      } catch (e) {
-        $.logErr(e, resp)
-      } finally {
-        resolve(data);
-      }
-    })
-    await $.wait(10000);
-    resolve()
-  })
-}
+//function readShareCode() {
+  //return new Promise(async resolve => {
+    //$.get({url: `http://jd.turinglabs.net/api/v2/jd/farm/read/${randomCount}/`, timeout: 10000,}, (err, resp, data) => {
+      //try {
+        //if (err) {
+          //console.log(`${JSON.stringify(err)}`)
+         // console.log(`${$.name} API请求失败，请检查网路重试`)
+        //} else {
+          //if (data) {
+            //console.log(`随机取个${randomCount}码放到您固定的互助码后面(不影响已有固定互助)`)
+            //data = JSON.parse(data);
+          //}
+        //}
+      //} catch (e) {
+        //$.logErr(e, resp)
+      //} finally {
+        //resolve(data);
+      //}
+    //})
+    //await $.wait(10000);
+    //resolve()
+  //})
+//}
 function shareCodesFormat() {
   return new Promise(async resolve => {
     // console.log(`第${$.index}个京东账号的助力码:::${jdFruitShareArr[$.index - 1]}`)
