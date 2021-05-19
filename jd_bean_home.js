cacf7e5b451de02a89e41276d24fbedbd5e45dec
xybhym <76042891+xybhym@users.noreply.github.com>
2021-05-19 16:22:08 +0800

    Update jd_bean_home.js

--- jd_bean_home.js
+++ jd_bean_home.js
@@ -27,7 +27,7 @@ const notify = $.isNode() ? require('./sendNotify') : '';
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭通知推送
 //IOS等用户直接用NobyDa的jd cookie
-let helpAuthor = true
+let helpAuthor = false
 let cookiesArr = [], cookie = '', message;
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
